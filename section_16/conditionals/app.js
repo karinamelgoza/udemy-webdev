8f1888e8b1c133c11e6f926e5779348efefc4ae6
Karina Melgoza <kmelgoza08@gmail.com>
2021-09-28 16:28:32 -0700

    Adds conditionals practice

---
+++ section_16/conditionals/app.js
@@ -0,0 +1,35 @@
+// let random = Math.random();
+
+// if (random < 0.5) {
+//     console.log("your numer is less than 0.5!")
+//     console.log(random)
+// }
+
+// if (random >= 0.5) {
+//     console.log('your number is greater than 0.5')
+//     console.log(random)
+// }
+
+// let dayOfWeek = 'Saturday';
+
+// if (dayOfWeek === 'Monday') {
+//     console.log('i hate mondays')
+// }
+// else if (dayOfWeek === 'Saturday') {
+//     console.log('yay its saturday')
+// }
+// else if (dayOfWeek === 'Friday') {
+//     console.log('fridays are fun')
+// }
+
+const age = 12;
+
+if (age < 5) {
+    console.log('you are a baby, you get in for free')
+}
+else if (age < 10) {
+    console.log('you are a child, you pay $10')
+}
+else if (age < 65) {
+    console.log('you are an adult, you pay $20')
+}
