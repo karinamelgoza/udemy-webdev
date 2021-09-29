21810b5783292195797cbe7c6f4c9ffeecaac690
Karina Melgoza <kmelgoza08@gmail.com>
2021-09-29 14:48:39 -0700

    Adds nested conditionals practice

--- section_16/conditionals/app.js
+++ section_16/conditionals/app.js
@@ -2,34 +2,52 @@
 
 // if (random < 0.5) {
 //     console.log("your numer is less than 0.5!")
-//     console.log(random)
 // }
-
-// if (random >= 0.5) {
+// else {
 //     console.log('your number is greater than 0.5')
-//     console.log(random)
 // }
+// console.log(random)
 
-// let dayOfWeek = 'Saturday';
+// let dayOfWeek = prompt('enter a day').toLowerCase();
 
-// if (dayOfWeek === 'Monday') {
+// if (dayOfWeek === 'monday') {
 //     console.log('i hate mondays')
 // }
-// else if (dayOfWeek === 'Saturday') {
+// else if (dayOfWeek === 'saturday') {
 //     console.log('yay its saturday')
 // }
-// else if (dayOfWeek === 'Friday') {
+// else if (dayOfWeek === 'friday') {
 //     console.log('fridays are fun')
 // }
+// else {
+//     console.log('meh')
+// }
 
-const age = 12;
+// const age = 76;
 
-if (age < 5) {
-    console.log('you are a baby, you get in for free')
+// if (age < 5) {
+//     console.log('you are a baby, you get in for free')
+// }
+// else if (age < 10) {
+//     console.log('you are a child, you pay $10')
+// }
+// else if (age < 65) {
+//     console.log('you are an adult, you pay $20')
+// }
+// else {
+//     console.log('you are a senior, you pay $10')
+// }
+
+const password = prompt('please enter a new password')
+
+if (password.length >= 6) {
+    if (password.indexOf(' ') === -1) {
+        console.log('valid password')
+    }
+    else {
+        console.log('password cannot contain spaces')
+    }
 }
-else if (age < 10) {
-    console.log('you are a child, you pay $10')
-}
-else if (age < 65) {
-    console.log('you are an adult, you pay $20')
+else {
+    console.log('password too short, must be 6+ characters')
 }
