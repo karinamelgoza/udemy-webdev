c5a9ab92cdc262d8063007c8d9fcdfc99e2ac240
Karina Melgoza <kmelgoza08@gmail.com>
2021-09-29 14:56:42 -0700

    Adds truthy/falsy practice

---
+++ section_16/truthy_falsy/app.js
@@ -0,0 +1,36 @@
+// const userInput = prompt('enter something');
+
+// if (userInput) {
+//     console.log('truthy')
+// }
+// else {
+//     console.log('falsy')
+// }
+
+// if (0) {
+//     console.log('truthy')
+// }
+// else {
+//     console.log('falsy')
+// }
+
+if (null) {
+    console.log('truthy')
+}
+else {
+    console.log('falsy')
+}
+
+if (NaN) {
+    console.log('truthy')
+}
+else {
+    console.log('falsy')
+}
+
+if (undefined) {
+    console.log('truthy')
+}
+else {
+    console.log('falsy')
+}
