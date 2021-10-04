2cbaac97d24b1bf40057bc3b24f063c7e21ca90c
Karina Melgoza <kmelgoza08@gmail.com>
2021-10-04 16:12:09 -0700

    Adds guessing game

--- section_19/while_loops/app.js
+++ section_19/while_loops/app.js
@@ -13,11 +13,37 @@
 // console.log('Congrats you got the secret!')
 
 
-let input = prompt('Hey, say something!')
-while (true) {
-    input = prompt(input)
-    if (input === 'stop copying me') {
+// let input = prompt('Hey, say something!')
+// while (true) {
+//     input = prompt(input)
+//     if (input === 'stop copying me') {
+//         break
+//     }
+// }
+// console.log('ok')
+
+let maximum = parseInt(prompt('Enter the maximum number!'))
+while (!maximum) {
+    maximum = parseInt(prompt('Enter a valid number!'))
+}
+
+const targetNum = Math.floor(Math.random() * maximum) + 1
+
+let guess = parseInt(prompt('Enter you first guess!'))
+let attempts = 1
+while (parseInt(guess) !== targetNum) {
+    if (guess === 'q') {
         break
     }
+    attempts++
+    if (guess > targetNum) {
+        guess = prompt('Too high! Guess again!')
+    } else {
+        guess = prompt('Too low! Guess again!')
+    }
 }
-console.log('ok')
+if (guess === 'q') {
+    console.log('You quit.')
+} else {
+    console.log(`You got it! It took you ${attempts} guesses.`)
+}
