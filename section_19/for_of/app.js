13a70f1d6afefef5bff29f0dc514fda043bbd367
Karina Melgoza <kmelgoza08@gmail.com>
2021-10-05 13:58:48 -0700

    Adds for of loops practice

---
+++ section_19/for_of/app.js
@@ -0,0 +1,9 @@
+// const subreddits = ['cringe', 'books', 'chickens', 'funny', 'pics', 'soccer']
+
+// for (let sub of subreddits) {
+//     console.log(sub)
+// }
+
+for (let char of 'hello world') {
+    console.log(char)
+}
