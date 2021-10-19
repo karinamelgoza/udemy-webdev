ee30cd305b969b080cb7382485ee402751c99e44
Karina Melgoza <kmelgoza08@gmail.com>
2021-10-19 14:21:56 -0700

    Adds forEach practice

--- section_22/for_each/app.js
+++ section_22/for_each/app.js
@@ -5,3 +5,26 @@ numbers.forEach(function(el){
         console.log(el)
     }
 })
+
+const movies = [
+    {
+        title: 'Amadeus',
+        score: 99
+    },
+    {
+        title: 'Stand By Me',
+        score: 85
+    },
+    {
+        title: 'Parasite',
+        score: 95
+    },
+    {
+        title: 'Alien',
+        score: 90
+    }
+]
+
+movies.forEach(function(movie){
+    console.log(`${movie.title}-${movie.score}/100`)
+})
