66313fa06db518255de2b67d0800f2f79831c70a
Karina Melgoza <kmelgoza08@gmail.com>
2021-10-21 13:05:13 -0700

    Adds setTimeout/setInterval practice

---
+++ section_22/set_timeout/app.js
@@ -0,0 +1,13 @@
+// console.log('hello')
+
+// setTimeout(()=>{
+//     console.log('are you still there?')
+// }, 3000)
+
+// console.log('goodbye')
+
+const id = setInterval(() => {
+    console.log(Math.random())
+}, 2000);
+
+// clearInterval(id)
