8a7c0afd8abb7b985a3ac1382914ebf4c1ca811b
Karina Melgoza <kmelgoza08@gmail.com>
2021-10-28 12:38:35 -0700

    Adds querySelector practice

--- section_24/Selecting/app.js
+++ section_24/Selecting/app.js
@@ -10,3 +10,9 @@ const squareImages = document.getElementsByClassName('square')
 for (let img of squareImages){
     img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg'
 }
+
+const links = document.querySelectorAll("p a")
+
+for (let link of links){
+    console.log(link.href)
+}
