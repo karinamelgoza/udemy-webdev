31606afdb079b305a2851e669b97a56f1ffb7df2
Karina Melgoza <kmelgoza08@gmail.com>
2021-10-29 11:55:07 -0700

    Adds setAttribute practice

--- section_24/manipulating/app.js
+++ section_24/manipulating/app.js
@@ -3,3 +3,9 @@ const allLinks = document.querySelectorAll('a')
 for(let link of allLinks){
     link.innerText = 'i am a link'
 }
+
+for(let link of allLinks){
+    link.style.color = 'rgb(0, 108, 134)'
+    link.style.textDecorationColor = 'magenta'
+    link.style.textDecorationStyle = 'wavy'
+}
