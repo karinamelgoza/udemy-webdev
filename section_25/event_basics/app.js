5b5764bb40a359ca685f8d0dcfc1d99929b00ea7
Karina Melgoza <kmelgoza08@gmail.com>
2021-11-08 13:59:37 -0700

    Adds eventListener practice

--- section_25/event_basics/app.js
+++ section_25/event_basics/app.js
@@ -14,3 +14,24 @@ btn.onmouseenter = scream
 document.querySelector('h1').onclick = () => {
     alert('hello')
 }
+
+const btn3 = document.querySelector('#v3')
+btn3.addEventListener('click', function(){
+    alert('clicked')
+})
+
+function twist() {
+    console.log('twist')
+}
+
+function shout() {
+    console.log('shout')
+}
+
+const tasBtn =  document.querySelector('#tas')
+
+// tasBtn.onclick = twist
+// tasBtn.onclick = shout
+
+tasBtn.addEventListener('click', twist, {once:true})
+tasBtn.addEventListener('click', shout)
