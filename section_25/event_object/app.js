cd9e00c7efac59cfa22c830c1a8fe5520fa8ea84
Karina Melgoza <kmelgoza08@gmail.com>
2021-11-11 12:18:24 -0700

    Adds event object practice

---
+++ section_25/event_object/app.js
@@ -0,0 +1,31 @@
+document.querySelector('button').addEventListener('click', function (evt) {
+    console.log(evt)
+})
+
+const input = document.querySelector('input')
+input.addEventListener('keydown', function(e) {
+    console.log(e.key)
+    console.log(e.code)
+})
+// input.addEventListener('keyup', function() {
+//     console.log('keyup')
+// })
+
+window.addEventListener('keydown', function (e) {
+    switch(e.code){
+        case 'ArrowUp':
+            console.log('up')
+            break
+        case 'ArrowDown':
+            console.log('down')
+            break
+        case 'ArrowRight':
+            console.log('right')
+            break
+        case 'ArrowLeft':
+            console.log('left')
+            break
+        default:
+            console.log('ignore')
+    }
+})
