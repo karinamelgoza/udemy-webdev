7e015f82f1591c87325cc3c3eb160e51619847c5
Karina Melgoza <kmelgoza08@gmail.com>
2021-11-29 12:37:38 -0700

    Adds bubbling and delegation practice

---
+++ section_25/bubbling/app.js
@@ -0,0 +1,17 @@
+const container = document.querySelector('#container')
+const button = document.querySelector('#changeColor')
+
+button.addEventListener('click', function(e){
+    container.style.backgroundColor = makeRandColor()
+    e.stopPropagation()
+})
+container.addEventListener('click', function(){
+    container.classList.toggle('hide')
+})
+
+const makeRandColor = () => {
+    const r = Math.floor(Math.random() * 255)
+    const g = Math.floor(Math.random() * 255)
+    const b = Math.floor(Math.random() * 255)
+    return `rgb(${r}, ${g}, ${b})`
+}
